2182d30d0801c7e65fd8df308a2e3e5d64e57218
Raul Miras Vidal <raulmvidal87@gmail.com>
2025-04-24 16:26:46 +0200

    feature: front followApi y useFollow

---
+++ frontend/src/api/internal/followApi.js
@@ -0,0 +1,29 @@
+import createApiClient from '../config/apiClient'; // Ajustá la ruta si es diferente
+
+// Seguir a un usuario
+export const followUser = async (followedId, token) => {
+  const apiClient = createApiClient(token);
+  const response = await apiClient.post(`/follows/${followedId}`);
+  return response.data;
+};
+
+// Dejar de seguir a un usuario
+export const unfollowUser = async (followedId, token) => {
+  const apiClient = createApiClient(token);
+  const response = await apiClient.delete(`/follows/${followedId}`);
+  return response.data;
+};
+
+// Obtener los seguidores de un usuario
+export const getFollowers = async (userId, token) => {
+  const apiClient = createApiClient(token);
+  const response = await apiClient.get(`/follows/followers/${userId}`);
+  return response.data;
+};
+
+// Obtener los usuarios que un usuario está siguiendo
+export const getFollowing = async (userId, token) => {
+  const apiClient = createApiClient(token);
+  const response = await apiClient.get(`/follows/following/${userId}`);
+  return response.data;
+};
