945e8fb2c1af4bdf34ddecab4d5ed00eb63ff902
Raul Miras Vidal <raulmvidal87@gmail.com>
2025-05-02 17:28:17 +0200

    fix: backend - usuarios get All, delete by ID

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -13,7 +13,7 @@ router.put('/update/:id', requireAuth, updateUser);
 router.post('/upload-profile-pic', requireAuth, upload.single('profilePic'), uploadProfilePic);
 
 
-router.get('/', requireAuth, requireAdmin, getAllUsers);  // Ruta para obtener todos los usuarios
+router.get('/', requireAuth, getAllUsers);  // Ruta para obtener todos los usuarios
 
 router.get('/me', requireAuth, getCurrentUser);
 
@@ -22,7 +22,7 @@ router.get('/id/:id', requireAuth, getUserById);
 router.get('/email/:email', requireAuth, getUserByEmail);
 
 
-// Borar por ID de Mongo
-router.delete('/:id', requireAuth, requireAdmin, deleteUser);
+// Borrar por ID de Mongo
+router.delete('/:id', requireAuth, deleteUser);
 
 module.exports = router;
