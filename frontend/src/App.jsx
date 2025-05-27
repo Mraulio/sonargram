a26134fe438a108e56e9f14a02e46aa974c11a12
Raul Miras Vidal <raulmvidal87@gmail.com>
2025-05-27 17:45:06 +0200

    fix: componente de TopFavoritosList y metido en /test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -26,7 +26,7 @@ import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 import i18n from './i18n';
 import Test from './pages/Test';
 import TestBuscador from './pages/TestBuscador';
-import TestTopFavoritos from './pages/TestTopFavoritos';
+import TestTopFavoritos from './components/TopFavoritosList';
 
 function App() {
   const { token } = useContext(UserContext);
@@ -46,7 +46,6 @@ function App() {
         <Route path="/followed" element={<PrivateRoute><FollowedPage/></PrivateRoute>} />
         <Route path="/test" element={<PrivateRoute><Test/></PrivateRoute>} />
         <Route path="/test2" element={<PrivateRoute><TestBuscador/></PrivateRoute>} />
-        <Route path="/test3" element={<PrivateRoute><TestTopFavoritos/></PrivateRoute>} />
 
         <Route path="*" element={<Navigate to={token ? "/dashboard" : "/"} />} />
       </Routes>
