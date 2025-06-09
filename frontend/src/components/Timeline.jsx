1da8d09703fda1f547b8df7a420e46300df7850a
Raul Miras Vidal <raulmvidal87@gmail.com>
2025-06-09 19:31:50 +0200

    fix: posible nuevo timeline con itemrow en el modal

--- frontend/src/components/Timeline.jsx
+++ frontend/src/components/Timeline.jsx
@@ -1,36 +1,43 @@
-// src/components/Timeline.js
+/* src/components/Timeline.js */
 import React, { useContext, useEffect } from 'react';
 import { UserContext } from '../context/UserContext';
 import useActivity from '../hooks/useActivity';
 import ActivityCard from './ActivityCard';
-import { Typography } from '@mui/material';
+import { Typography, Box } from '@mui/material';
+import useRatings from '../hooks/useRatings';
+import useFavorites from '../hooks/useFavorites';
 
 const Timeline = () => {
   const { token } = useContext(UserContext);
   const { activities, loading, error, fetchTimeline } = useActivity(token);
+  const ratingProps = useRatings(token);
+  const favoriteProps = useFavorites(token);
 
   useEffect(() => {
     if (token) fetchTimeline();
   }, [token, fetchTimeline]);
 
-  if (loading) return <p>Cargando actividades...</p>;
-  if (error) return <p className="text-red-500">Error: {error}</p>;
+  if (loading) return <Typography>Cargando actividades...</Typography>;
+  if (error) return <Typography color="error">Error: {error}</Typography>;
 
   return (
-    <div>
-  {activities.length === 0 ? (
-    <Typography>No hay actividades recientes.</Typography>
-  ) : (
-    <>
-      <Typography variant="h5" gutterBottom>Timeline</Typography>
-      {activities.map((activity) => (
-        <ActivityCard key={activity._id} activity={activity} />
-      ))}
-    </>
-  )}
-</div>
-
-
+    <Box>
+      {activities.length === 0 ? (
+        <Typography>No hay actividades recientes.</Typography>
+      ) : (
+        <>
+          <Typography variant="h5" gutterBottom>Timeline</Typography>
+          {activities.map((activity) => (
+            <ActivityCard
+              key={activity._id}
+              activity={activity}
+              ratingProps={ratingProps}
+              favoriteProps={favoriteProps}
+            />
+          ))}
+        </>
+      )}
+    </Box>
   );
 };
 
